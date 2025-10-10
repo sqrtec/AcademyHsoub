5d171a7fe2f00ab374a8b5dd7ea95c7f975f2613
Saker Chaher <sakerammar00@gmail.com>
2025-10-10 09:18:55 +0300

    Finsh from basic website

---
+++ Company_Website/src/js/index.js
@@ -0,0 +1,6 @@
+// هنا يتم أستدعاء جميع المكتبات
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css'
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min.js'
+import '../sass/style.scss'
---
+++ Company_Website/webpack.config.js
@@ -0,0 +1,94 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development", // أو "production" عند البناء النهائي
+    entry: "./src/js/index.js",
+    output: {
+        filename: "js/bundle.js",
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/",
+        clean: true, // ينظف مجلد build تلقائياً قبل كل بناء
+    },
+
+    module: {
+        rules: [
+        {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                publicPath: "../",
+                },
+            },
+            "css-loader",
+            "sass-loader",
+            ],
+        },
+
+        {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            type: "asset/resource",
+            generator: {
+            filename: "images/[name][ext]",
+            },
+        },
+
+        {
+            test: /\.(eot|woff|woff2|ttf)$/i,
+            type: "asset/resource",
+            generator: {
+            filename: "fonts/[name][ext]",
+            },
+        },
+
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+
+        {
+            test: require.resolve("jquery"),
+            loader: "expose-loader",
+            options: {
+            exposes: ["$", "jQuery"],
+            },
+        },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        }),
+        new MiniCssExtractPlugin({
+        filename: "css/style.css",
+        }),
+    ],
+
+    // Webpack Server
+    devServer: {
+        static: {
+        directory: path.join(__dirname, "build"),
+        },
+        port: 9000,
+        open: true,
+        compress: true,
+        client: {
+        logging: "none",
+        overlay: true, // يُظهر الأخطاء في المتصفح
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+    },
+
+    performance: {
+        hints: false,
+    },
+
+    stats: "errors-only",
+};
